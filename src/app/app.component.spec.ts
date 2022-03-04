6a2be0f9b735df75e0fd622e22403f20ad071798
phoomin2012 <phoomin009@gmail.com>
2022-03-04 22:27:22 +0700

    Add search box and table show categories

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,22 +10,5 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'refinitiv-q2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('refinitiv-q2');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('refinitiv-q2 app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CategoryService } from './category.service';
+import { Observable } from 'rxjs';
+import { ICategoryResponse } from 'src/types';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'refinitiv-q2';
+
+  constructor(private categoryService: CategoryService) { }
+
+  filter = new FormControl('')
+  list: string[] = [];
+
+  async fetchData() {
+    return fetch('https://api.publicapis.org/categories')
+  }
+
+  ngOnInit(): void {
+    this.getCategory()
+  }
+
+  getCategory(): void {
+    this.categoryService.getCategory().subscribe((response: ICategoryResponse) => this.list = response.categories);
+  }
+
+  get filteredList() {
+    const regex = new RegExp(this.filter.value)
+    console.log(this.list)
+    return this.list.filter(category => regex.test(category))
+  }
 }
